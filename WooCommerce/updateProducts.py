525db5490850be319f7a08774eafcd47492833fc
toobiwankenobi <tobias.hoch@bluewin.ch>
2024-02-12 19:47:54 +0100

    create update function and improve existing ones

--- WooCommerce/updateProducts.py
+++ WooCommerce/updateProducts.py
@@ -4,6 +4,7 @@ import os
 import csv
 import subprocess
 import pandas as pd
+from datetime import datetime
 
 # current working directory of this file
 current_directory = os.getcwd()
@@ -25,23 +26,18 @@ wcapi = API(
  # Get the current working directory
 csv_file_name = "conda.csv" # Specify the CSV file name
 csv_file_path = os.path.join(current_directory, csv_file_name) # Create the full path to the CSV file
-df = pd.read_csv(csv_file_path) # Read the CSV file into a DataFrame
-
-# Check if column "id" exists
-if 'id' not in df.columns:
-    # If it doesn't exist, create a new column with no entries
-    df['id'] = None
+df = pd.read_csv(csv_file_path)
 
 # loop through rows
 for index, row in df.iterrows():
-    if pd.isnull(row['id']): # only continue if project does not exist yets
+    if not pd.isnull(row['id']) and row["published"] == 1: # only create product if there is no ID yet
         # Access values of each column for the current row
         data = {
         "name": row["name"],
         "meta_data": [
             {
             "key": "wpneo_funding_minimum_price",
-            "value": row[" min_investment"]
+            "value": row["min_investment"]
             },
             {
             "key": "krowd_project_link",
@@ -49,7 +45,6 @@ for index, row in df.iterrows():
             }
         ],
         "type": "crowdfunding",
-        "published": 1,
         "images": [
             {"src": row["wpImageLink"],
              "id": row["wpImageID"]}
@@ -57,14 +52,16 @@ for index, row in df.iterrows():
         }
 
         # upload 
-        result = wcapi.post("products", data)
-        result = result.json()
+        update = wcapi.put(f"products/{row['id']}" , data).json()
         
         # add ID to newProducts object
-        df.at[index, 'id'] = result['id']
-        print(f"INFO: new entry created. ID: {result['id']}")
-    else:
-        print("INFO: entry already exists")
+        df.at[index, 'lastUpdate'] = datetime.now()
+        print(f"INFO: project updated. ID: {row['id']}")
+    if not pd.isnull(row['id']) and row["published"] == 0: # delete project if status was set to 0
+        delete = wcapi.delete(f"products/{row['id']}", params={"force": True}).json() # delete
+        print(f"INFO: project deleted. ID: {row['id']}")
+        df.at[index, 'id'] = None # remove id from csv
+
 
 # save updated csv file
 df.to_csv(csv_file_path, index=False)
