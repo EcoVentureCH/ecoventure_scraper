7742463bae9045fb96a25c64e876c42e58caacc6
yhofmann <yhofmann@student.ethz.ch>
2024-06-20 14:53:57 +0200

    yannic_more try

--- scraper_definitions.py
+++ scraper_definitions.py
@@ -20,6 +20,7 @@ scraper definitions:
 
     see scraper_api for useful functions (not documented yet!)
 '''
+import logging
 
 import time
 import re
@@ -56,6 +57,7 @@ def conda():
 
     return project_datas
 
+'''
 
 def seedrs():
 
@@ -78,7 +80,7 @@ def seedrs():
     
     
     print("seedrs test start")
-    print(html_project_page)
+ #   print(html_project_page)
     
     
 
@@ -92,15 +94,15 @@ def seedrs():
     
     return project_datas
 
-
+'''
 def republic():
     data_to_extract = {
         'name':           lambda bfs: sc.text_from_class(bfs, 'h1', 'h2 favourite'),
         'external_link':  lambda bfs: sc.text_from_class(bfs, 'meta', 'og:url', 'property', key='content'),
         'image':          lambda bfs: sc.text_from_class(bfs, 'meta', 'og:image', 'property', key='content'),
-        'min_investment': lambda bfs: sc.number_from_class(bfs, 'tr', 'share-price'),
-        'funding_target': lambda bfs: sc.number_from_class(bfs, 'div', 'investment_total_target'),
-        'progress':       lambda bfs: sc.number_from_class(bfs, 'div', 'CampaignProgress-text')
+        'min_investment': lambda bfs: sc.number_from_class(bfs, 'div', 's-marginTop0_5 u-textCenter s-fontSize14 u-fontWeight400 u-colorMuted'),
+        'funding_target': lambda bfs: sc.number_from_class(bfs, 'div', 'u-fontWeight600 u-textRight s-marginLeft1 offerings-show-flexible_deal_terms-item__value'),
+        'progress':       lambda bfs: sc.number_from_class(bfs, 'div', 's-marginBottom1_5 s-borderBottom1_5 offerings-show-header-raised_amount')
     }
     
     print('republic test start')
@@ -121,7 +123,7 @@ def republic():
     
     html_project_page = sc.get_html()
     
-    print(html_project_page)
+    #print(html_project_page)
     regex_projects_urls = r'<a href=\"(.+)\" itemscope>'
      #r'<p\s+class=["\']OfferingCardContent-module__title___DrRba["\']\s+itemProp=["\']name["\']>([^<]+)<\/p>' #r'<a href=\"(.+)\" itemscope>'
     
@@ -145,14 +147,43 @@ def republic():
     
 
 def econeers():
+    def text_from_class(bfs, tag, class_name):
+        try:
+            element = bfs.find(tag, class_=class_name)
+            if element:
+                return element.get_text(strip=True)
+            else:
+                print('element not found')
+                return None
+            
+        except AttributeError:
+            return None
+
+    def text_from_meta(bfs, tag, attr, value, key='content'):
+        print(bfs.find_all(tag, {attr: value}))
+        try:
+            return bfs.find_all(tag, {attr: value})
+        except (AttributeError, TypeError):
+            print("Error finding text from meta")
+            return None
+
+    def number_from_class(bfs, tag, class_name):
+        try:
+            text = bfs.find_all(tag, class_=class_name).get_text(strip=True)
+            return int(re.sub(r'[^\d]', '', text))
+        except (AttributeError, ValueError):
+            print("Error finding number from class")
+            return None
+        
+ 
+
     data_to_extract = {
-        'name':           lambda bfs: sc.text_from_class(bfs, 'h1', 'h2 favourite'),
-        'external_link':  lambda bfs: sc.text_from_class(bfs, 'meta', 'og:url', 'property', key='content'),
-        'image':          lambda bfs: sc.text_from_class(bfs, 'meta', 'og:image', 'property', key='content'),
-        'min_investment': lambda bfs: sc.number_from_class(bfs, 'tr', 'share-price'),
-        'funding_target': lambda bfs: sc.number_from_class(bfs, 'div', 'investment_total_target'),
-        'progress':       lambda bfs: sc.number_from_class(bfs, 'div', 'CampaignProgress-text')
-    
+        #'name': lambda bfs: sc.text_from_class(bfs, 'meta',   'og:description', 'property', key='content'),
+        'external_link':  lambda bfs: sc.text_from_class(bfs, 'meta',  'og:url', 'property', key='content'),
+        'image':  lambda bfs: sc.text_from_class(bfs, 'meta',   'og:image', 'property', key='content'),
+        'min_investment': lambda bfs: number_from_class(bfs, 'td', 'tg-kftd'),
+        'funding_target': lambda bfs: number_from_class(bfs, 'td', 'tg-lqy6'),
+        'progress': lambda bfs: number_from_class(bfs, 'h2', 'center-text')
     }
     
     print('econeers start')
@@ -161,11 +192,25 @@ def econeers():
     sc.open(url)
 
     html_project_page = sc.get_html()
-    regex_projects_urls = r'<a href="(/projekte/.*?)">'
-
+    regex_projects_urls = r'<a (href="https:\/\/www.econeers.de\/investmentchancen\/.*?)"'
+    
+    project_urls = re.findall(regex_projects_urls, html_project_page, re.MULTILINE)
+    project_urls = ['https://www.econeers.de' + url for url in project_urls]
+    
+    print("data to extract")
+    print(data_to_extract)
+    print("project urls")
+    print(project_urls)
+    
+    project_datas = sc.scrape_projects(data_to_extract, project_urls)
+    
+    print("econeers end")
+    
+    return project_datas
 
 if __name__ == "__main__":
     with sc.scraper_context():
         #conda()
-        seedrs()
-        republic()
+       # seedrs()
+        #republic()
+        econeers()
